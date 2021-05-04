fa580a2e247999748c510fe1fd5b21b1cd3d6e04
Nurlailiyah Salsabilah Valentina <salsabilavalen@student.telkomuniversity.ac.id>
2021-05-03 20:14:58 -0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if2092/galerihewan/model/Hewan.kt
+++ app/src/main/java/org/d3if2092/galerihewan/model/Hewan.kt
@@ -1,7 +1,9 @@
 package org.d3if2092.galerihewan.model
 
+import org.d3if2092.galerihewan.R
+
 data class Hewan (
         val nama: String,
         val namaLatin: String,
-        val imageResId: Int
+        val imageResId: Int = R.drawable.angsa
 )
